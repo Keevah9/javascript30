a2b95f91cb07288949ffec11d97091b89c109b0d
Ogunniyi Veronica <86062530+Keevah9@users.noreply.github.com>
2022-07-15 10:34:26 +0100

    new

--- city-filter/js/main.js
+++ city-filter/js/main.js
@@ -1,53 +1,80 @@
-
-
+const inputs = document.querySelector('input')
+let formContent = document.querySelector('.formContent')
 const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const cities = [] 
+//fetch api
+//filter city by name
+//if user input, pop up the city
+//sort by rank or distance
+//map 
 
-    fetch(url)
-    .then(res => res.json())
-    .then(data => cities.push(...data)) 
+const cities = []
 
-function findMatch(word, cities){
+fetch(url)
+.then(res => res.json())
+.then(data => cities.push(...data))
+console.log(cities)
+
+//filtering based on a keyword
+const findMatch = ((word, cities)=>{
     return cities.filter(place =>{
-        const regex = new RegExp(word, 'gi')//global and  insensitivity
+        let regex = new RegExp (word, 'gi')
 
-        let correct  = place.city.match(regex)  || place.state.match(regex)
-
-        return correct
+        let current = place.city.match(regex) || place.state.match(regex)
+        
+        return current
     })
     
-}
+})
 
-function numberWithCommas(x) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+let commas = (num)=> num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
-function displayMatch(){
-    const matchItems = findMatch(this.value, cities)
+function checkMatch(){
+    let matchItems = findMatch(this.value, cities)
     console.log(matchItems)
-    matchItems.sort((a, b) =>{
-        return b.rank - a.rank
+
+    matchItems.sort((a,b)=>{
+       return a.latitude - b.latitude
     })
-    const content = matchItems.map( place =>{
-        const regex = new RegExp(this.value, 'gi')
-        const cityName = place.city.replace(regex, `<span class="hLight"> ${this.value}</span>`)
-        const stateName = place.state.replace(regex, `<span class="hLight"> ${this.value}</span>`)
 
-    return ` <p> 
-                <span>${cityName}, ${stateName}</span>
-                <span>${numberWithCommas(place.population)}</span>
-            </p>`
-        
+    
+    formContent.innerHTML =  matchItems.map(place =>{
+        //replace value with new colored value
+        let regex = new RegExp (this.value, 'gi')
+        let cityName = place.city.replace(regex, `<span class="hLight">${this.value} </span>`)
+        let stateName = place.city.replace(regex, `<span class="hLight">${this.value} </span>`)
+
+
+
+        return `
+             <p> 
+              <span>${cityName}, ${stateName}</span>
+                <span>${commas(place.population)}</span>
+         </p>  
+        `
     }).join('')
-    formContent.innerHTML = content
+    // formContent.innerHTML = content 
 }
+    
 
 
-const span = document.querySelector('.span')
-const input = document.querySelector('input')
-const formContent = document.querySelector('.formContent')
+inputs.addEventListener('change', checkMatch)
+inputs.addEventListener('keyup', checkMatch)
 
 
-input.addEventListener('change', displayMatch)
-input.addEventListener('keyup', displayMatch)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
